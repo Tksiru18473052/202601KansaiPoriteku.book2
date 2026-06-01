9a25601f5c90be241748bdddedecc844a84a2570
sameda kousaku <sameda.kousaku@example.com>
2026-06-01 13:27:43 +0900

    ログイン画面を編集

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,12 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-6">
+    <!-- ヘッダー -->
+    <header class="py-5 border-bottom"
+        style="background-color: #eceae6;">
+        <div class="container">
+            <div class="container d-flex justify-content-between align-items-center">
+            <h3 class="mb-0 text-secondary fw-bold">株式会社　ポリポリどっとこむ</h3>
+            <span class="fs-3 text-secondary fw-bold">社員専用ページです（部外者立ち入り禁止）</span>
+        </div>
+    </header>
+
+    <div class="row justify-content-center" style="margin-top: 15vh;">
+            <div class="col-md-4">
                 <div class="card">
                     <div class="card-header text-center">
                         <h4>書籍管理システム</h4>
+    
                     </div>
                     <div class="card-body">
                         @if ($errors->any())
